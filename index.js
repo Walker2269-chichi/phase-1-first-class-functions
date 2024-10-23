254250eca493a203d0769f9fa577e44112e64a7a
Walker2269-chichi <langatjeremiah22@gmail.com>
2024-10-23 20:24:52 +0300

    First class function

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    function fn() {
+        return "first class";
+    }
+
+    return fn;
+}
+function returnsAnAnonymousFunction() {
+    return function() {
+        return "before all";
+    }
+}
